eb3ba7a086aa52449efd682f02cf4a1dc1cc8dac
tgmarinho <tgmarinho@gmail.com>
2012-09-19 19:01:30 -0400

    insert e delete ok

--- src/dao/GenericDao.java
+++ src/dao/GenericDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -20,6 +21,8 @@ public class GenericDao {
 	private String user;
 	private String password;
 	private String driver;
+	private String sql;
+	private ResultSet rs;
 
 	public GenericDao() {
 
@@ -37,30 +40,94 @@ public class GenericDao {
 		}
 	}
 
-	public void inserirFuncionario() {
-
+	public void inserirFuncionario(Funcionario funcionario) {
+		try {
+			sql = "INSERT INTO funcionario VALUES (?,?,?)";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, funcionario.getId());
+			ps.setString(2, funcionario.getNome());
+			ps.setInt(3, funcionario.getSetor().getId());
+			ps.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void inserirSetor() {
-
+	public void inserirSetor(Setor setor) {
+		try {
+			sql = "INSERT INTO setor VALUES (?,?,?)";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, setor.getId());
+			ps.setString(2, setor.getNome());
+			ps.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void deletarFuncionario(Funcionario funcionario) {
-
+		try {
+			sql = "DELETE FROM funcionario WHERE id=?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, funcionario.getId());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void deletarSetor(Setor setor) {
-
+		try {
+			sql = "DELETE FROM setor WHERE id=?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, setor.getId());
+			ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public List<Funcionario> buscarTodosFuncionarios() {
 
-		return new ArrayList<Funcionario>();
+		try {
+			st = conn.createStatement();
+			sql = "SELECT * FROM funcionario";
+			rs = st.executeQuery(sql);
+
+			List<Funcionario> listaFuncionarios = new ArrayList<Funcionario>();
+
+			while (rs.next()) {
+				Funcionario funcionario = new Funcionario();
+				funcionario.setSetor(new Setor());
+				funcionario.setId(rs.getInt("id"));
+				funcionario.setNome(rs.getString("nome"));
+				funcionario.getSetor().setId(rs.getInt("id_setor"));
+				listaFuncionarios.add(funcionario);
+			}
+			return listaFuncionarios;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public List<Setor> buscarTodosSetores() {
 
-		return new ArrayList<Setor>();
+		try {
+			st = conn.createStatement();
+			sql = "SELECT * FROM setor";
+			rs = st.executeQuery(sql);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 
 }
